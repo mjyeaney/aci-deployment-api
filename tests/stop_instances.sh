7c941d88f3c5104769eae8065ac3123383373d61
mjyeaney <michael.yeaney@gmail.com>
2019-01-16 13:32:14 -0500

    Added better messages to test scripts

--- tests/stop_instances.sh
+++ tests/stop_instances.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # This script simply stops all instances (but does not delete) - update as needed
-ACI_API_HOST='http://localhost:8009/api/deployments'
+ACI_API_HOST="http://localhost:8009/api/deployments"
 
 instances=$(curl -s $ACI_API_HOST | jq -r '.[].name')
 
 for i in $instances; 
 do 
+    echo -n "Stopping $i..."
     curl -X POST -H 'Content-length: 0' $ACI_API_HOST/$i/stop
+    echo "done."
 done
