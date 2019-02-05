0e8daff1d312301c25fe2d797acf84faef3abdea
mjyeaney <michael.yeaney@gmail.com>
2019-02-04 21:40:14 -0500

    Adding unit tests for matching strategy

--- src/tests/default-matching-strategy-tests.ts
+++ src/tests/default-matching-strategy-tests.ts
@@ -5,6 +5,7 @@
 import * as dotenv from "dotenv";
 import { before } from "mocha";
 import * as assert from "assert";
+import { ContainerGroup } from "azure-arm-containerinstance/lib/models";
 import { IGroupMatchingStrategy } from "../common-types";
 import { DefaultMatchingStrategy } from "../default-matching-strategy";
 
@@ -17,5 +18,71 @@ describe("Default Matching Strategy methods", () => {
         sut = new DefaultMatchingStrategy();
     });
 
-    
+    it("Builds image name with tag if specified", () => {
+        let imageName = sut!.GetImageName("foo", "bar");
+        assert.equal(imageName, "foo:bar");
+    });
+
+    it("Does not append tag specifier if none specified", () => {
+        let imageName = sut!.GetImageName("foo", undefined);
+        assert.equal(imageName, "foo");
+    });
+
+    it("Determines terminated state based on ContainerGroup state", () => {
+        let fakeGroup = {
+            identity: undefined,
+            provisioningState: "sample",
+            containers: [],
+            imageRegistryCredentials: undefined,
+            restartPolicy: "Never",
+            ipAddress: undefined,
+            osType: "windows",
+            volumes: undefined,
+            instanceView: {
+                events: [],
+                state: "terminated"
+            },
+            diagnostics: undefined,
+            networkProfile: undefined,
+            dnsConfig: undefined
+        };
+
+        let isTerminated: boolean = sut!.IsTerminated(fakeGroup);
+        assert.equal(isTerminated, true);
+
+        fakeGroup.instanceView.state = "no so much";
+        isTerminated = sut!.IsTerminated(fakeGroup);
+        assert.equal(isTerminated, false);
+    });
+
+    it("Determines terminated state regardless of state casing", () => {
+        let fakeGroup = {
+            identity: undefined,
+            provisioningState: "sample",
+            containers: [],
+            imageRegistryCredentials: undefined,
+            restartPolicy: "Never",
+            ipAddress: undefined,
+            osType: "windows",
+            volumes: undefined,
+            instanceView: {
+                events: [],
+                state: "TeRmInAtEd"
+            },
+            diagnostics: undefined,
+            networkProfile: undefined,
+            dnsConfig: undefined
+        };
+
+        let isTerminated: boolean = sut!.IsTerminated(fakeGroup);
+        assert.equal(isTerminated, true);
+
+        fakeGroup.instanceView.state = "TERMINATED";
+        isTerminated = sut!.IsTerminated(fakeGroup);
+        assert.equal(isTerminated, true);
+
+        fakeGroup.instanceView.state = "tErMiNaTeD";
+        isTerminated = sut!.IsTerminated(fakeGroup);
+        assert.equal(isTerminated, true);
+    });
 });
